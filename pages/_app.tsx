2f84d03d5755bac1e80c4cf64e9d0e074543e36b
Paolo Manaois <paolo.manaois@yahoo.fr>
2023-01-10 19:08:24 +0100

    wagmi config

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,17 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { createClient, WagmiConfig } from 'wagmi';
+import { providers } from 'ethers';
+
+const web3Client = createClient({
+  autoConnect: false,
+  provider: new providers.JsonRpcProvider(process.env.NETWORK, parseInt(String(process.env.CHAIN_ID)))
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <WagmiConfig client={web3Client}>
+      <Component {...pageProps} />
+    </WagmiConfig>
+  );
 }
